980ac402e718ff5c42145bfcb46da5b8bb9e24a2
Jing Jin <54995030+jin00058@users.noreply.github.com>
2020-10-05 00:15:32 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//addition funcation
+function add(a, b){
+        return a + b
+    }
+    //subtraction funcation
+    function subtract(a, b){
+        return a - b
+    }
+    //multiplication funcation
+    function multiply(a, b){
+        return a * b
+    }
+    //division function
+    function divide(a, b){
+        return a / b
+    }
+    //big long calculation
+    let result = add(  subtract(1, 2) , multiply(3 ,4)  )
